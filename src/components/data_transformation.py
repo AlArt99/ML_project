54eb738f0f3397fd808ad054cf93056b775e52c0
AlArt99 <AlArt1991@gmail.com>
2024-08-20 20:05:39 +0200

    Prediction Pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -31,13 +31,13 @@ class DataTransformation:
         """
 
         try:
-            numeric_columns = ["writing score", "reading score"]
+            numeric_columns = ["writing_score", "reading_score"]
             categorical_columns = [
                 "gender",
-                "race/ethnicity",
-                "parental level of education",
+                "race_ethnicity",
+                "parental_level_of_education",
                 "lunch",
-                "test preparation course",
+                "test_preparation_course",
             ]
 
             num_pipeline = Pipeline(
@@ -80,8 +80,8 @@ class DataTransformation:
 
             preprocessing_obj = self.get_data_transformer_object()
 
-            target_column_name = "math score"
-            numercial_columns = ["writing score", "reading score"]
+            target_column_name = "math_score"
+            numercial_columns = ["writing_score", "reading_score"]
 
             input_feature_train_df = train_df.drop(target_column_name, axis=1)
             target_feature_train_df = train_df[target_column_name]
